1021f26103585d3a017919493506dc0f921d0fc7
Narek Keshishyan <narek.keshishyan@disqo.com>
2023-03-02 01:35:40 +0400

    - Add language support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageEnum, LanguageShortEnum } from './core/enums/language.enum';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,20 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
+  selectedLanguage = LanguageEnum.ENGLISH;
 
-  constructor() {
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang(LanguageShortEnum.EN);
+    translate.use(LanguageShortEnum.EN);
+  }
+
+  setLanguage(lang: string) {
+    this.translate.use(lang);
+    if (lang === LanguageShortEnum.EN) {
+      this.selectedLanguage = LanguageEnum.ENGLISH;
+    } else if (lang === LanguageShortEnum.GR) {
+      this.selectedLanguage = LanguageEnum.GREEK;
+    }
   }
 
   ngOnInit() {
---
+++ src/app/core/enums/language.enum.ts
@@ -0,0 +1,9 @@
+export enum LanguageEnum {
+  ENGLISH = 'English',
+  GREEK = 'Greek'
+}
+
+export enum LanguageShortEnum {
+  EN = 'en',
+  GR = 'gr'
+}
